52d8310561ff90324e64b0faa6c33533e6f6bc66
Chaekang Lee <leechis7@gmail.com>
2024-01-31 16:32:29 +0900

    Initial commit

---
+++ src/main/java/com/sqisoft/springbootdeveloper/TestService.java
@@ -0,0 +1,16 @@
+package com.sqisoft.springbootdeveloper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TestService {
+    @Autowired
+    MemberRepository memberRepository; // Bean 주입
+
+    public List<Member> getAllMembers() {
+        return memberRepository.findAll(); // 멤머 목록 얻기
+    }
+}
