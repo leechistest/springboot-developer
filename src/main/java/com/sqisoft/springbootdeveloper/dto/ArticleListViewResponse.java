5497e91642be47de3b3e95b636c8b5e66552c99a
Chaekang Lee <leechis7@gmail.com>
2024-03-26 20:10:26 +0900

    7장 완료

---
+++ src/main/java/com/sqisoft/springbootdeveloper/dto/ArticleListViewResponse.java
@@ -0,0 +1,17 @@
+package com.sqisoft.springbootdeveloper.dto;
+
+import com.sqisoft.springbootdeveloper.domain.Article;
+import lombok.Getter;
+
+@Getter
+public class ArticleListViewResponse {
+    private final Long id;
+    private final String title;
+    private final String content;
+
+    public ArticleListViewResponse(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+}
